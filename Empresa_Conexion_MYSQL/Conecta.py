294836660dd8224efc347076618c23dd63f0bed1
Carlos Ignacio <whitebownitro@gmail.com>
2024-10-30 09:36:20 -0300

    Add Se agrega menu en Conecta

--- Empresa_Conexion_MYSQL/Conecta.py
+++ Empresa_Conexion_MYSQL/Conecta.py
@@ -2,8 +2,28 @@
 # y ejecución de los métodos de la clase
 
 from Database import *
+from os import system
 
 db = Database()
-db.ListaRepuestos()
-db.BuscarRepuesto()
-db.cerrarBD()
+
+
+while True:
+    elige = input('\n Elije una opcion: \n\
+        \t Mostrar un repuesto(u)\n\
+        \t Mostrar todos los repuestos(t)\n\
+        \t Fin(f)\n\
+        \t ==>').lower()
+    if elige == 'u':
+        codAbuscar = input('Ingrese código a buscar =')
+        db.BuscarRepuesto(codAbuscar)
+    elif elige == 't':
+        db.ListaRepuestos()
+    elif elige == 'f':
+        print('Fin')
+        db.cerrarBD()
+        break
+    else:
+        print('Error de opción')
+        
+    input('Pulse Enter para continuar...')
+    system('cls')
